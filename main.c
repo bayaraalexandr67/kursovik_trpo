01b694572946400fa7966e3afcd0879dcb6da28b
bayara <boyarkin-alexander55@yandex.ru>
2020-06-13 21:00:52 +0700

    Добавлен счётчик времени

--- main.c
+++ main.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
+#include <sys/time.h>
 
 #define MAX_ARR_LEN 10
 #define MAX_STR_LEN (MAX_ARR_LEN-1)
+
+double wtime()
+{
+    struct timeval t;
+    gettimeofday(&t, NULL);
+    return (double)t.tv_sec + (double)t.tv_usec * 1E-6;
+}
 
 int check(char *correct_word, char *entered_word)
 {
@@ -29,17 +37,21 @@ void printstr(char *s)
 int main()
 {
     int uncorrect_sym_count;
+    double start_time, end_time, time_delta;
     printf("КЛАВИАТУРНЫЙ ТРЕНАЖЁР\n");
     printf("Пожалуйста, продублируйте слово: \n");
     char word[MAX_ARR_LEN] = "abybucker";
     char entered_word[MAX_ARR_LEN];
     printstr(&word);
+    start_time = wtime();
     fgets(entered_word, MAX_STR_LEN, stdin);
+    end_time = wtime();
+    time_delta = end_time - start_time;
     uncorrect_sym_count = check(&word, &entered_word);
     if(uncorrect_sym_count == 0){
-        printf("Вы ввели Правильное слово!\n");
+        printf("Вы ввели Правильное слово! Потрачено времени: %f сек.\n", time_delta);
     } else {
-        printf("Вы ввели неправильно %d букв!\n", uncorrect_sym_count);
+        printf("Вы ввели неправильно %d букв! Потрачено времени: %f сек.\n", uncorrect_sym_count, time_delta);
     }
     return 0;
 }
