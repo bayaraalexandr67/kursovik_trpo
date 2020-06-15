f7366811ecef89be479624f014c76532174eac64
bayara <boyarkin-alexander55@yandex.ru>
2020-06-15 20:34:44 +0700

    Добавлено главное меню

--- main.c
+++ main.c
@@ -1,7 +1,18 @@
+#include <stdio.h>
 #include "game.h"
 
 int main()
 {
-    game();
+    int select;
+    printf("КЛАВИАТУРНЫЙ ТРЕНАЖЁР\n");
+    printf("=====================\n");
+    printf("1) Начать игру\n");
+    printf("2) Выйти из игры\n");
+    printf("\n");
+    printf("Выберите действие: ");
+    scanf("%d%*c", &select);
+    if(select == 1){
+        game();
+    }
     return 0;
 }
