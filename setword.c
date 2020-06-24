93f5aa9b9c9c677df88fca3288bb6aa7f022d075
bayara <boyarkin-alexander55@yandex.ru>
2020-06-24 20:24:21 +0700

    Добавлены новые слова

--- setword.c
+++ setword.c
@@ -7,99 +7,94 @@ void set_word(char *str, int max_lenght)
 {
     srand(time(NULL));
     int value;
-    value = ((rand() % 8)+1);
+    value = ((rand() % 9)+1);
     switch(value){
     case 1:
-        str[0] = 'F';
-        str[1] = 'u';
-        str[2] = 'l';
-        str[3] = 'm';
+        str[0] = 'o';
+        str[1] = 'l';
+        str[2] = 'd';
+        str[3] = 'f';
         str[4] = 'a';
-        str[5] = 'n';
+        str[5] = 'g';
         str[6] = '\0';
         break;
     case 2:
-        str[0] = 'P';
-        str[1] = 'u';
-        str[2] = 'd';
-        str[3] = 'o';
-        str[4] = 'v';
-        str[5] = '\0';
+        str[0] = 'c';
+        str[1] = 'a';
+        str[2] = 'l';
+        str[3] = 'c';
+        str[4] = 'u';
+        str[5] = 'l';
+        str[6] = 'a';
+        str[7] = 't';
+        str[8] = 'e';
+        str[9] = '\0';
         break;
     case 3:
-        str[0] = 'B';
-        str[1] = 'e';
-        str[2] = 'r';
-        str[3] = 'l';
-        str[4] = 'i';
-        str[5] = 'z';
-        str[6] = 'o';
-        str[7] = 'v';
-        str[8] = '\0';
-        break;
-    case 4:
-        str[0] = 'Y';
+        str[0] = 's';
         str[1] = 't';
-        str[2] = 'u';
-        str[3] = 'p';
-        str[4] = 'i';
-        str[5] = 'n';
+        str[2] = 'a';
+        str[3] = 't';
+        str[4] = 'u';
+        str[5] = 's';
         str[6] = '\0';
         break;
+    case 4:
+        str[0] = 'd';
+        str[1] = 'e';
+        str[2] = 'f';
+        str[3] = 'a';
+        str[4] = 'u';
+        str[5] = 'l';
+        str[6] = 't';
+        str[7] = '\0';
+        break;
     case 5:
-        str[0] = 'C';
-        str[1] = 'h';
-        str[2] = 'u';
-        str[3] = 'd';
-        str[4] = 'i';
-        str[5] = 'n';
-        str[6] = 'o';
-        str[7] = 'v';
-        str[8] = '\0';
+        str[0] = 'b';
+        str[1] = 'r';
+        str[2] = 'a';
+        str[3] = 'k';
+        str[4] = 'e';
+        str[5] = '\0';
         break;
     case 6:
-        str[0] = 'A';
-        str[1] = 'v';
-        str[2] = 'd';
-        str[3] = 'u';
-        str[4] = 'k';
-        str[5] = 'o';
-        str[6] = 'v';
-        str[7] = '\0';
+        str[0] = 'p';
+        str[1] = 'r';
+        str[2] = 'i';
+        str[3] = 'm';
+        str[4] = 'e';
+        str[5] = 'r';
+        str[6] = '\0';
         break;
     case 7:
-        str[0] = 'P';
-        str[1] = 'e';
-        str[2] = 's';
-        str[3] = 'h';
-        str[4] = 'k';
-        str[5] = 'o';
-        str[6] = 'v';
-        str[7] = '\0';
-        break;
-    case 8:
-        str[0] = 'K';
+        str[0] = 'c';
         str[1] = 'o';
         str[2] = 'n';
-        str[3] = 'o';
-        str[4] = 'v';
-        str[5] = 'a';
-        str[6] = 'l';
-        str[7] = 'o';
-        str[8] = 'v';
-        str[9] = '\0';
+        str[3] = 't';
+        str[4] = 'i';
+        str[5] = 'n';
+        str[6] = 'u';
+        str[7] = 'e';
+        str[8] = '\0';
         break;
-    case 9:
-        str[0] = 'A';
-        str[1] = 'i';
+    case 8:
+        str[0] = 'o';
+        str[1] = 'b';
         str[2] = 'r';
         str[3] = 'a';
-        str[4] = 'p';
+        str[4] = 'z';
         str[5] = 'e';
-        str[6] = 't';
-        str[7] = 'j';
-        str[8] = 'n';
-        str[9] = '\0';
+        str[6] = 'c';
+        str[7] = '\0';
+        break;
+    case 9:
+        str[0] = 'n';
+        str[1] = 'e';
+        str[2] = 'w';
+        str[3] = 'f';
+        str[4] = 'u';
+        str[5] = 'g';
+        str[6] = '\0';
         break;
     default:
         str[0] = 'e';
