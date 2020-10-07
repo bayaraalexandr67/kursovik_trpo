efec779490b03f643058e5f19e0938f707b0372e
bayara <boyarkin-alexander55@yandex.ru>
2020-10-07 20:59:17 +0700

    Исправлена ошибка подсчёта неправильных букв

--- check.c
+++ check.c
@@ -1,12 +1,38 @@
+int strln(char *string)
+{
+    int len = 0;
+    while(string[len] != '\0' && string[len] != '\n'){
+        len += 1;
+    }
+    return len;
+}
+
+int checkln(char *first_string, char *second_string)
+{
+    int first_len = strln(first_string);
+    int second_len = strln(second_string);
+    return (first_len - second_len);
+}
+
 int check(char *correct_word, char *entered_word)
 {
+    int test;
     int i = 0;
+    int flag = 0;
     int uncorrect_symbs = 0;
-    while(correct_word[i] != '\0' && entered_word[i] != '\0'){
-        if(entered_word[i] != correct_word[i]){
-            uncorrect_symbs += 1;
+    test = checkln(correct_word, entered_word);
+    if(test != 0){
+        if(test < 0){
+            test = (test * (-1));
         }
-        i += 1;
+        return test;
+    } else {
+        while(correct_word[i] != '\0' && entered_word[i] != '\0'){
+            if(entered_word[i] != correct_word[i]){
+                uncorrect_symbs += 1;
+            }
+            i += 1;
+        }
     }
     return uncorrect_symbs;
 }
--- check.h
+++ check.h
@@ -2,5 +2,7 @@
 #define CHECK_H
 
 int check(char *correct_word, char *entered_word);
+int checkln(char *first_string, char *second_string);
+int strln(char *string);
 
 #endif
