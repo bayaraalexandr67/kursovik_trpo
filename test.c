c04a4cb67d44686f2bfef6b187cc4bb2c80a1376
bayara <boyarkin-alexander55@yandex.ru>
2020-10-07 21:01:08 +0700

    Добавлены тесты для функций strln и checkln

--- test.c
+++ test.c
@@ -1,6 +1,42 @@
 #include "ctest.h"
 #include "check.h"
-
+//STRLN
+CTEST(string_lenght, zero_symbol_str) {
+    int n;
+    char str[4];
+    str[0] = '\0';
+    n = strln(str);
+    ASSERT_EQUAL(0, n);
+}
+CTEST(string_lenght, one_symbol_str) {
+    int n;
+    n = strln("a");
+    ASSERT_EQUAL(1, n);
+}
+CTEST(string_lenght, standart_str) {
+    int n;
+    n = strln("basasasasa");
+    ASSERT_EQUAL(10, n);
+}
+CTEST(string_lenght, slash_n_trigger) {
+    int n;
+    n = strln("basas\nasasa");
+    ASSERT_EQUAL(5, n);
+}
+//CHECKLN
+CTEST(check_string_lenght, biggest_lenght) {
+    int n = checkln("abu", "ab");
+    ASSERT_EQUAL(1, n);
+}
+CTEST(check_string_lenght, equivalent_lenght) {
+    int n = checkln("abu", "abu");
+    ASSERT_EQUAL(0, n);
+}
+CTEST(check_string_lenght, least_lenght) {
+    int n = checkln("ab", "abu");
+    ASSERT_EQUAL(-1, n);
+}
+//CHECK
 CTEST(word_check, right_word) {
     int incorrect_symbols;
     incorrect_symbols = check("hckas", "hckas");
